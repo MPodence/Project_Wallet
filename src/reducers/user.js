395473952f698b3a5ef3efecec2fc37f6658ee1b
Podence <podence23@gmail.com>
2022-06-24 18:31:33 -0300

    o teste nao suporta o estado iniciando nulo, entao tive que arrumar e perdi um tempinho, eu nao sabia...

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,10 @@
 import { SAVE_EMAIL } from '../actions';
 
-const user = (state = null, action) => { // o proprio erro no console me deu a dica de iniciar como null caso eu nao queira valor, ja que nao é aceito undefined.
+const INITIAL_STATE = {
+  email: '', // string que armazena o email da pessoa usuária
+}
+
+const user = (state = INITIAL_STATE, action) => { // o proprio erro no console me deu a dica de iniciar como null caso eu nao queira valor, ja que nao é aceito undefined.
   switch (action.type) {
   case SAVE_EMAIL:
     return {
